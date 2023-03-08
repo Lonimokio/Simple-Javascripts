0e0175ff76204f3b113e9da937619ba9566f096d
Arttu Mutka <Arttu.mutka@gmail.com>
2023-03-08 12:39:08 +0200

    Fixed Filter script, renamed it and also commented

---
+++ Filter_Hide.js
@@ -0,0 +1,74 @@
+<script>
+
+// Below here is the main part of the code. Please don't touch without some understanding of JavaScript.
+
+//Variables
+var MIteration = 0;
+// Functions
+// Function for hiding element
+function hide() {
+  // Adding elements into array to be hidden
+  const elements = document.getElementsByClassName("FilteringOptions__option-wrapper");
+    
+    if (MIteration == 0)
+    {
+      // Hiding elements
+      for (const el of elements){
+            el.style.display= "none";
+      }
+      //Showing certain filters
+      let elementsToShow =  document.getElementsByClassName("FilteringOptions__option-wrapper FilteringOptions__option-wrapper--collections[Koko] col-12 col-sm-6 col-md-4 col-xl-3");
+      for (const el of elementsToShow){
+        el.style.display = "block";
+      }
+      elementsToShow = document.getElementsByClassName("FilteringOptions__option-wrapper FilteringOptions__option-wrapper--price col-12 col-sm-6 col-md-4 col-xl-3");
+      for (const el of elementsToShow){
+          el.style.display = "block";
+      }
+    }
+    else
+    {
+      //Showing all elements
+      for (const el of elements){
+          el.style.display= "block";
+      }
+	  }
+
+    //Changing iteration
+    if (MIteration == 0)
+    {
+      MIteration = 1;
+    }
+    else
+    {
+      MIteration = 0;
+    }
+
+    
+};
+
+// Main code loop in a function
+function mainFunction() {
+  // Toggle on mobile
+  if (window.innerWidth <= 800) {
+    // Hiding filters
+    document.getElementsByClassName("FilteringOptions__heading btn btn-link")[0].click();
+    hide();
+    // Creating button
+    const button = document.createElement("button");
+    button.innerHTML = "Show Filters";
+    button.onclick = function() {
+      hide();
+    };
+    // Adding button to the page
+    const container = document.getElementsByClassName("MainProductList__active-filter-row")[0]; 
+    container.appendChild(button);
+
+	  
+}
+}
+
+//Calling main code loop with a delay to let the page load
+setTimeout(mainFunction, 3000);
+
+</script>
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Simple-Javascripts
-Here i have a collection of simple javascripts I have made. Feel free to adapt them to your needs
+# Simple-javascript's
+
+ Here I have a collection of simple javascript's I have made. Feel free to adapt them to your needs
+
+ List of current scripts:
+    -Filter_Hide: Filter Hide is a simple script I made to hide certain filters and show others when the screen is too narrow. It is meant to unclutter a webstore on a mobile device.
