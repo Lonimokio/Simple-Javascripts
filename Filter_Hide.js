6114ffbe0d5854b143ccc38160eeb3f04176a60a
Arttu Mutka <Arttu.mutka@gmail.com>
2023-03-13 10:14:31 +0200

    Updated filtered search to be more user friendly

--- Filter_Hide.js
+++ Filter_Hide.js
@@ -1,5 +1,17 @@
 <script>
 
+//Here are the variables you can change. Please don't touch the code below this part.
+
+var ButtonLocation = "MainProductList__active-filter--quicksearch mr-lg-auto col-lg-8 col-xl-6"; // The class name of the element where the button will be added. You can find it by right clicking the element and clicking "Inspect". The class name will be in the "class" attribute. You can also use the "id" attribute by changing the "className" to "id" in the code below. If you don't know what to do, leave it as it is.
+var ButtonText = "Show filters"; // The text that will be displayed on the button.
+
+var HideElementLocation = "FilteringOptions__option-wrapper"; // The class name of the element that will be hidden. You can find it by right clicking the element and clicking "Inspect". The class name will be in the "class" attribute. You can also use the "id" attribute by changing the "className" to "id" in the code below. If you don't know what to do, leave it as it is.
+
+var ShowElementLocation1 = "FilteringOptions__option-wrapper FilteringOptions__option-wrapper--collections[Koko] col-12 col-sm-6 col-md-4 col-xl-3"; // The class name of the element that will be shown. You can find it by right clicking the element and clicking "Inspect". The class name will be in the "class" attribute. You can also use the "id" attribute by changing the "className" to "id" in the code below. If you don't know what to do, leave it as it is.
+var ShowElementLocation2 = "FilteringOptions__option-wrapper FilteringOptions__option-wrapper--price col-12 col-sm-6 col-md-4 col-xl-3"; // The class name of the element that will be shown. You can find it by right clicking the element and clicking "Inspect". The class name will be in the "class" attribute. You can also use the "id" attribute by changing the "className" to "id" in the code below. If you don't know what to do, leave it as it is.
+var ButtonToClick = "FilteringOptions__heading btn btn-link"; // The class name of the element that will be clicked. You can find it by right clicking the element and clicking "Inspect". The class name will be in the "class" attribute. You can also use the "id" attribute by changing the "className" to "id" in the code below. If you don't know what to do, leave it as it is.
+
+
 // Below here is the main part of the code. Please don't touch without some understanding of JavaScript.
 
 //Variables
@@ -8,7 +20,7 @@ var MIteration = 0;
 // Function for hiding element
 function hide() {
   // Adding elements into array to be hidden
-  const elements = document.getElementsByClassName("FilteringOptions__option-wrapper");
+  const elements = document.getElementsByClassName(HideElementLocation);
     
     if (MIteration == 0)
     {
@@ -17,11 +29,11 @@ function hide() {
             el.style.display= "none";
       }
       //Showing certain filters
-      let elementsToShow =  document.getElementsByClassName("FilteringOptions__option-wrapper FilteringOptions__option-wrapper--collections[Koko] col-12 col-sm-6 col-md-4 col-xl-3");
+      let elementsToShow =  document.getElementsByClassName(ShowElementLocation1);
       for (const el of elementsToShow){
         el.style.display = "block";
       }
-      elementsToShow = document.getElementsByClassName("FilteringOptions__option-wrapper FilteringOptions__option-wrapper--price col-12 col-sm-6 col-md-4 col-xl-3");
+      elementsToShow = document.getElementsByClassName(ShowElementLocation2);
       for (const el of elementsToShow){
           el.style.display = "block";
       }
@@ -52,16 +64,16 @@ function mainFunction() {
   // Toggle on mobile
   if (window.innerWidth <= 800) {
     // Hiding filters
-    document.getElementsByClassName("FilteringOptions__heading btn btn-link")[0].click();
+    document.getElementsByClassName(ButtonToClick)[0].click();
     hide();
     // Creating button
     const button = document.createElement("button");
-    button.innerHTML = "Show Filters";
+    button.innerHTML = ButtonText;
     button.onclick = function() {
       hide();
     };
     // Adding button to the page
-    const container = document.getElementsByClassName("MainProductList__active-filter-row")[0]; 
+    const container = document.getElementsByClassName(ButtonLocation)[0]; 
     container.appendChild(button);
 
 	  
@@ -70,5 +82,7 @@ function mainFunction() {
 
 //Calling main code loop with a delay to let the page load
 setTimeout(mainFunction, 3000);
+
+//Intelectual property of Arttu Mutka. Some rights reserved.
 
 </script>
